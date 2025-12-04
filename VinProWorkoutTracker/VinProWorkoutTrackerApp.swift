ca541ffa2c4cf95a2063ac3a4620135f815736b7
Vinay Viswambharan <vinay_vv@hotmail.com>
2025-12-03 22:02:39 -0800

    Initial Commit

---
+++ VinProWorkoutTracker/VinProWorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinProWorkoutTrackerApp.swift
+//  VinProWorkoutTracker
+//
+//  Created by Vinays Mac on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct VinProWorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VinProWorkoutTrackerTests/VinProWorkoutTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VinProWorkoutTrackerTests.swift
+//  VinProWorkoutTrackerTests
+//
+//  Created by Vinays Mac on 12/3/25.
+//
+
+import Testing
+@testable import VinProWorkoutTracker
+
+struct VinProWorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
